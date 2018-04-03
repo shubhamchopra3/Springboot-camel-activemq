cc399f64740785029f72bd8f7047b375e2fb51e0
Shubham Chopra <shubhamchopra553@gmail.com>
2018-04-03 16:30:03 +0530

    Update SampleAutowiredAmqRoute.java

--- springboot-camel-activemq/src/main/java/sample/camel/SampleAutowiredAmqRoute.java
+++ springboot-camel-activemq/src/main/java/sample/camel/SampleAutowiredAmqRoute.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleAutowiredAmqRoute extends RouteBuilder {
-
+String msg="Hello from Camel";
     @Override
     public void configure() throws Exception {
         from("activemq:sample.queue")
             .to("log:sample");
 
         from("timer://foo?repeatCount=1")     // use from("timer://foo?repeatCount=1")  to send only once the route starts and use  from("timer:bar") to send every second
-        .setBody(constant("Hello from Camel"))
+        .setBody(constant(msg))
         .to("activemq:sample.queue");      //sample.queue is the name of queue
     }
 
